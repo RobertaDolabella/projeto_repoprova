6c5ef4c9b36a38cc297ec92f65cd805c4fab343e
RobertaDolabella <dolabella.roberta@gmail.com>
2022-09-17 10:02:36 -0300

    inicio do projeto. Estruturação inicial para o server e banco de dados criado. Proximo passo deploy com heroku ligado ao github.

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express, {json} from "express";
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+
+const app = express()
+
+dotenv.config()
+app.use(json())
+
+// app.use(router)
+// app.use(errorMiddleware)
+
+
+const PORT = Number(process.env.PORT) || 5000;
+app.listen(PORT, () => {
+  console.log(`Servidor funfando de boas na porta: ${PORT}`);
+});
